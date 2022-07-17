68f5a88371b5b0f7d80369eda052a0b0aff5f122
Leonardo Almeida <leoalmeidasa@gmail.com>
2022-07-17 10:45:46 -0300

    refactor(Gemfile): executado bundle update

--- introduction_rspec/spec/mocks/mocks_spec.rb
+++ introduction_rspec/spec/mocks/mocks_spec.rb
@@ -1,33 +1,28 @@
 require "student"
 
-describe "Mocks" do
+describe Student do
+  subject { described_class.new }
   it '#bar' do
-    # Setup
-    student = Student.new
-
     # Verify
-    expect(student).to receive(:bar)
+    expect(subject).to receive(:bar)
 
     # Exercise
-    student.bar
+    subject.bar
   end
 
   it 'args' do
-    student = Student.new
-    expect(student).to receive(:foo).with(123)
-    student.foo(123)
+    expect(subject).to receive(:foo).with(123)
+    subject.foo(123)
   end
 
   it 'repetição' do
-    student = Student.new
-    expect(student).to receive(:foo).with(123).twice
-    student.foo(123)
-    student.foo(123)
+    expect(subject).to receive(:foo).with(123).twice
+    subject.foo(123)
+    subject.foo(123)
   end
 
   it 'retorno' do
-    student = Student.new
-    expect(student).to receive(:foo).with(123).and_return(true)
-    student.foo(123)
+    expect(subject).to receive(:foo).with(123).and_return(true)
+    subject.foo(123)
   end
 end
